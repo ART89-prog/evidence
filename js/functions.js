4ec850fd23b8aa88cef4e0ac64100092c323cf59
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2023-02-08 12:36:56 +0300

    445645

--- js/functions.js
+++ js/functions.js
@@ -52,30 +52,30 @@ $(() => {
 		utilsScript: "js/utils.js",
 	}));
 
-	// var input = document.querySelector("#phone2");
-	// if (window.intlTelInput(input, {
-	// 	// allowDropdown: false,
-	// 	autoHideDialCode: true,
-	// 	//   autoPlaceholder: "off",
-	// 	// dropdownContainer: document.body,
-	// 	// excludeCountries: ["us"],
-	// 	// formatOnDisplay: false,
-	// 	//   geoIpLookup: function(callback) {
-	// 	//     $.get("http://ipinfo.io", function() {}, "jsonp").always(function(resp) {
-	// 	//       var countryCode = (resp && resp.country) ? resp.country : "";
-	// 	//       callback(countryCode);
-	// 	//     });
-	// 	//   },
-	// 	// hiddenInput: "full_number",
-	// 	//   initialCountry: "auto",
-	// 	// localizedCountries: { 'de': 'Deutschland' },
-	// 	nationalMode: false,
-	// 	// onlyCountries: ['us', 'gb', 'ch', 'ca', 'do'],
-	// 	// placeholderNumberType: "MOBILE",
-	// 	// preferredCountries: ['cn', 'jp'],
-	// 	// separateDialCode: true,
-	// 	utilsScript: "js/utils.js",
-	// }));
+	var input2 = document.querySelector("#phone2");
+	if (window.intlTelInput(input2, {
+		// allowDropdown: false,
+		autoHideDialCode: true,
+		//   autoPlaceholder: "off",
+		// dropdownContainer: document.body,
+		// excludeCountries: ["us"],
+		// formatOnDisplay: false,
+		//   geoIpLookup: function(callback) {
+		//     $.get("http://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+		//       var countryCode = (resp && resp.country) ? resp.country : "";
+		//       callback(countryCode);
+		//     });
+		//   },
+		// hiddenInput: "full_number",
+		//   initialCountry: "auto",
+		// localizedCountries: { 'de': 'Deutschland' },
+		nationalMode: false,
+		// onlyCountries: ['us', 'gb', 'ch', 'ca', 'do'],
+		// placeholderNumberType: "MOBILE",
+		// preferredCountries: ['cn', 'jp'],
+		// separateDialCode: true,
+		utilsScript: "js/utils.js",
+	}));
 
 
 
